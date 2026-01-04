0c3316c5cced0875c12fd39a11577a85842171ce
Code Stark <158564186+Ashfak-Kausik@users.noreply.github.com>
2026-01-05 00:33:05 +0600

    Trying parameter validation with return keyword

--- dialogue.js
+++ dialogue.js
@@ -1,6 +1,13 @@
-let width = prompt("Volume of the box, enter width",0);
-let height = prompt("Volume of the box, enter height",0);
-let length = prompt("Volume of the box, enter length",0);
-let volume = width * height * length;
+function getMeanTemp(temperatures) {
+    if (!(temperatures instanceof Array)) {
+        return NaN;
+    }
+    let sum = 0;
+    for (i = 0; i < temperatures.length; i++) {
+        sum += temperatures[i];
+    }
+    return sum / temperatures.length;
+}
 
-console.log(`The volume of the box is ${volume}`);
+console.log(getMeanTemp(23, 25, 29));
+console.log(getMeanTemp([23, 25, 29, 22, 24, 26, 28, 27, 30, 31, 21, 20, 19, 18, 17]));
