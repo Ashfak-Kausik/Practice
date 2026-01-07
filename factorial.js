7c1a33e2899ae436fa5cfbd8b0673b376d2f8bcc
Code Stark <158564186+Ashfak-Kausik@users.noreply.github.com>
2026-01-08 00:02:22 +0600

    Learning functions

---
+++ factorial.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+    let result = 1;
+    while (n > 1) {
+        result *= n;
+        n--;
+    }
+    return result;
+}
+console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(6));
